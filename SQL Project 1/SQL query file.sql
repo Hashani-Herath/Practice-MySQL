1580970f5cac115966bf6de3b3617715a31a480a
Hashani-Herath <nadeesha01ngc@gmail.com>
2026-05-12 11:10:15 +0530

    Add new staging table for enhanced data organization and duplicate handling

--- SQL Project 1/SQL query file.sql
+++ SQL Project 1/SQL query file.sql
@@ -90,3 +90,46 @@ WHERE (company, location, industry, total_laid_off, percentage_laid_off, `date`,
 -- so let's do it!!
 
 ALTER TABLE world_layoffs.layoffs_staging ADD row_num INT;
+
+SELECT *
+FROM world_layoffs.layoffs_staging;
+
+CREATE TABLE `world_layoffs`.`layoffs_staging2` (
+`company` text,
+`location`text,
+`industry`text,
+`total_laid_off` INT,
+`percentage_laid_off` text,
+`date` text,
+`stage`text,
+`country` text,
+`funds_raised_millions` int,
+row_num INT
+);
+
+INSERT INTO `world_layoffs`.`layoffs_staging2`
+(`company`,
+`location`,
+`industry`,
+`total_laid_off`,
+`percentage_laid_off`,
+`date`,
+`stage`,
+`country`,
+`funds_raised_millions`,
+`row_num`)
+SELECT `company`,
+`location`,
+`industry`,
+`total_laid_off`,
+`percentage_laid_off`,
+`date`,
+`stage`,
+`country`,
+`funds_raised_millions`,
+		ROW_NUMBER() OVER (
+			PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions
+			) AS row_num
+	FROM 
+		world_layoffs.layoffs_staging;
+
