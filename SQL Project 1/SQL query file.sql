995f85d74b6f38f8c7c87427669fd77e28989a7a
Hashani-Herath <nadeesha01ngc@gmail.com>
2026-05-12 11:05:59 +0530

    Refactor delete logic to ensure accurate removal of duplicates in layoffs_staging table

--- SQL Project 1/SQL query file.sql
+++ SQL Project 1/SQL query file.sql
@@ -73,5 +73,15 @@ WHERE
 	row_num > 1
 )
 DELETE
-FROM DELETE_CTE
-;
+FROM DELETE_CTE;
+
+WITH DELETE_CTE AS (
+	SELECT company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions, 
+    ROW_NUMBER() OVER (PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
+	FROM world_layoffs.layoffs_staging
+)
+DELETE FROM world_layoffs.layoffs_staging
+WHERE (company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions, row_num) IN (
+	SELECT company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions, row_num
+	FROM DELETE_CTE
+) AND row_num > 1;
