fb4167a8ac69e5cac0395a28fbb685a2a187ff35
Hashani-Herath <nadeesha01ngc@gmail.com>
2026-05-19 11:26:02 +0530

    Add query to order companies by funds raised for 100% layoffs analysis

--- SQL Project 2/EDA.sql
+++ SQL Project 2/EDA.sql
@@ -22,3 +22,10 @@ SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1;
 -- these are mostly startups it looks like who all went out of business during this time
+
+-- if we order by funcs_raised_millions we can see how big some of these companies were
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE  percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+-- BritishVolt looks like an EV company, Quibi! I recognize that company - wow raised like 2 billion dollars and went under - ouch
